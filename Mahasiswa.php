c4f1ff82b27dbe27a44eae72f8cde7d85b3d4c5b
Muhammad Irvan <mhmdirvn@student.untan.ac.id>
2021-02-18 22:47:36 +0700

    pratikum 02 membuat objek dan class

---
+++ Mahasiswa.php
@@ -0,0 +1,17 @@
+<?php
+Class Mahasiswa
+{
+    public $nim;
+    public $nama;
+    public $tanggal_lahir;
+    public $jenis_kelamin;
+
+    public function tampilkanAngkatan()
+    {
+        
+    }
+     public function tampilkanUmur()
+     {
+
+     }
+}
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include('Mahasiswa.php');
+$pramudia = new Mahasiswa();
+$rafi = new Mahasiswa();
+$naufal = new Mahasiswa();
+
+$pramudia->nim = 'H1101191062';
+$pramudia->nama = 'Muhammad Pramudia Irvan Chaniago';
+$pramudia->tanggal_lahir = '05 Mei 2001';
+$pramudia->jenis_kelamin = 'Laki - laki';
+
+$rafi->nim = 'H1101191030';
+$rafi->nama = 'Rafi Hardi';
+$rafi->tanggal_lahir = '23 Januari 2001';
+$rafi->jenis_kelamin = 'Laki - laki';
+
+$naufal->nim = 'H1101191004';
+$naufal->nama = 'Naufal Zahran';
+$naufal->tanggal_lahir = '30 Mei 2001';
+$naufal->jenis_kelamin = 'Laki - laki';
+ 
